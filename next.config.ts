68a7fe16f4c83766204d52adb750e4b048186d78
zulqarnain-codeblue <zulqarnain.codeblue@gmail.com>
2025-11-25 11:29:50 +0500

    updated the tsconfig.json as next.config didn't ignore the video/hls folder

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {}, // 👈 This removes the Turbopack warning
-
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.ts$/,
-      include: /public\/videos\/hls/,
-      type: "asset/resource",
-    });
-
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
